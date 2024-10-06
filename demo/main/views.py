b381377c754b5c3839ffc82ba7692458ebc0f817
Tim Heithecker <tim.heithecker@gmail.com>
2024-10-06 23:20:50 +0200

    Improve pagination

--- demo/main/views.py
+++ demo/main/views.py
@@ -4,5 +4,4 @@ from pagetools.views import PaginatorMixin
 
 
 class ArticleListView(PaginatorMixin):
-    paginate_by = 5
     model = Article
--- pagetools/views.py
+++ pagetools/views.py
@@ -1,6 +1,7 @@
 import re
 
 from django.conf import settings
+from django.core.paginator import Page, Paginator
 from django.views.generic.list import ListView
 
 
@@ -9,16 +10,23 @@ class PaginatorMixin(ListView):
     Paginator Implementation
     """
 
-    paginate_by = getattr(settings, "PAGINATE_BY", 20)
+    paginate_by = getattr(settings, "PAGINATE_BY", 10)
+    num_pagelinks = getattr(settings, "PAGINATION_NUM_PAGELINKS", 5)
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
-        page = context["page_obj"]
-        paginator = page.paginator
-        _from = page.number - 5 if page.number > 5 else 0
-        context["curr_page_range"] = paginator.page_range[_from : page.number + 5]
-        url_for_page = self.request.get_full_path()
+        page: Page = context["page_obj"]
+        paginator: Paginator = page.paginator
+        halfrange = self.num_pagelinks // 2
+        _from = max(0, page.number - halfrange - 1)
+        if page.number > paginator.num_pages - halfrange:
+            _from = max(0, _from - (halfrange - (paginator.num_pages - page.number)))
+        until = halfrange + page.number
+        if page.number <= halfrange:
+            until += halfrange - page.number + 1
+        context["curr_page_range"] = paginator.page_range[_from:until]
 
+        url_for_page = self.request.get_full_path()
         if cpy := self.request.GET.copy():
             if cpy.pop("page", None):
                 url_for_page = re.sub(r"[\?|&]page=\d+", "", url_for_page)
